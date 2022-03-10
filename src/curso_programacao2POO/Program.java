bf8d0e5185a8c62c5105732783e926a416fc800f
Filipe Fernandes <filipefirmino50@gmail.com>
2022-03-10 12:16:30 -0300

    constructs1

--- src/curso_programacao2POO/Program.java
+++ src/curso_programacao2POO/Program.java
@@ -12,32 +12,42 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product prod = new Product();
+		//Product prod = new Product();
 		
 		System.out.println("Enteder product data: ");
 		System.out.print("Name: ");
-		prod.name = sc.nextLine();
+		String name = sc.nextLine();
+		//prod.name = sc.nextLine();
 		
 		System.out.print("Price: ");
-		prod.price = sc.nextDouble();
+		double price = sc.nextDouble();
+		//prod.price = sc.nextDouble();
 		
-		System.out.print("Quantity: ");
-		prod.quantity = sc.nextInt();
+		System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt();
+		//prod.quantity = sc.nextInt();
+		System.out.println();
+		
+		Product prod = new Product(name,price,quantity);
 		
 		System.out.println("Product data: " + prod);
+		System.out.println();
 		
 		System.out.print("Enter the new number of products to be added in stock: ");
 			int num1 = sc.nextInt();
 			prod.addProducts(num1);
 			
+		System.out.println();	
 		System.out.println("Update data: " + prod);
 		//System.out.println("Product data: " + prod.name + ", $ " + prod.price +
 		//		", " + prod.quantity + "units, Total: $ " + prod.totalValueInStock());
 		
+		System.out.println();
 		System.out.print("Enter the new number of products to be removed from stock: ");
 			int num2 = sc.nextInt();
 			prod.removeProducts(num2);
-		
+			
+		System.out.println();
 		System.out.println("Update data: " + prod);
 		//System.out.println("Product data: " + prod.name + ", $ " + prod.price +
 		//		", " + prod.quantity + "units, Total: $ " + prod.totalValueInStock());
